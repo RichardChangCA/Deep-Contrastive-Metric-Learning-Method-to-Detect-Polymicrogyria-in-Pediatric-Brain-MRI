a82cf320b07099be146142228490b3dab182155e
Lingfeng(Rinn) Zhang <41410483+RichardChangCA@users.noreply.github.com>
2023-09-29 02:00:21 -0400

    Update run.sh

--- PPMR/run.sh
+++ PPMR/run.sh
@@ -8,19 +8,19 @@ test_list="13 23 34 6"
 #inner fold 0
 val_list="4 2 5 19"
 
-python3 metric_learning_classification.py --test="13 23 34 6" --val="4 2 5 19" --inner_fold=0
+python3 ResNet50_without_NAN_metric_learning_classification.py --test="13 23 34 6" --val="4 2 5 19" --inner_fold=0
 
 #inner fold 1
 val_list="31 29 3 28"
 
-python3 metric_learning_classification.py --test="13 23 34 6" --val="31 29 3 28" --inner_fold=1
+python3 ResNet50_without_NAN_metric_learning_classification.py --test="13 23 34 6" --val="31 29 3 28" --inner_fold=1
 
 #inner fold 2
 val_list="30 20 8 17"
 
-python3 metric_learning_classification.py --test="13 23 34 6" --val="30 20 8 17" --inner_fold=2
+python3 ResNet50_without_NAN_metric_learning_classification.py --test="13 23 34 6" --val="30 20 8 17" --inner_fold=2
 
 #inner fold 3
 val_list="10 18 14 16"
 
-python3 metric_learning_classification.py --test="13 23 34 6" --val="10 18 14 16" --inner_fold=3
+python3 ResNet50_without_NAN_metric_learning_classification.py --test="13 23 34 6" --val="10 18 14 16" --inner_fold=3
